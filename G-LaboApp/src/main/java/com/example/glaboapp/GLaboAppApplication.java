6cbc6c7130cd2a2e6ce18df5f661f4b0364fc6b1
G-labo <pipi.mustang7852@gmail.com>
2021-04-07 13:12:23 +0900

    G-LaboApp first Commit!

---
+++ G-LaboApp/src/main/java/com/example/glaboapp/GLaboAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.glaboapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GLaboAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GLaboAppApplication.class, args);
+	}
+
+}
---
+++ G-LaboApp/src/test/java/com/example/glaboapp/GLaboAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.glaboapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GLaboAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
